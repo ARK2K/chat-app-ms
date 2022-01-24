b15ff52853e8ae5e11690c91a8835c85bda1eb8c
Ark2K <ankurravindrakushwaha@gmail.com>
2022-01-24 19:08:17 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const projectID = "a7b218fe-ba4d-4be9-ba94-874b3edd28eb";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
